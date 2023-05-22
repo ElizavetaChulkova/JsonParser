aa3cb099e115bb3a5cd2c4b41844428a8cf38632
Elizaveta Chulkova <chulkova_liza@mail.ru>
2023-05-23 01:19:27 +0300

    Refactoring

--- src/main/java/chulkova/parser/App.java
+++ src/main/java/chulkova/parser/App.java
@@ -1,6 +1,6 @@
 package chulkova.parser;
 
-import chulkova.parser.models.Value;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -10,12 +10,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
-/**
- * Hello world!
- */
 public class App {
 
     public static ObjectMapper mapper = new ObjectMapper();
@@ -23,47 +18,33 @@ public class App {
     public static void main(String[] args) throws IOException {
         System.out.println("Please, enter path to tests.json");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String file1 = br.readLine(); //reading paths to JSON files
+        String fileTests = br.readLine();
         System.out.println("Please, enter path to values.json");
-        String file2 = br.readLine();
-//получила список объектов tests.json
-        String dataFromFile1 = FileDataReader.read(file1);
-//        List<TestItem> testItem = new ObjectMapper().readValue(dataFromFile1, TestItem.class).getTests();
+        String fileValues = br.readLine();
 
-//получила список объектов values.json
-        String dataFromFile2 = FileDataReader.read(file2);
-        List<Value> value = new ObjectMapper().readValue(dataFromFile2, Value.class).getValues();
-        Map<Integer, String> valuesMap = convertListToMap(value);
+        List<JsonNode> testsArray = mapper.readTree(new File(fileTests))
+                .findParents("id");
+        List<JsonNode> valuesArray = mapper.readTree(new File(fileValues))
+                .findParents("id");
 
-        //получить ноды объектов tests.json
-        JsonNode root = mapper.readTree(new File(file1));
-        List<JsonNode> array = root.findParents("id");
-
-        List<JsonNode> valuesArray = mapper.readTree(new File(file2)).findParents("id");
-
-        JsonNode rootNode = mapper.readValue(dataFromFile1, JsonNode.class);
-        rootNode = rootNode.findParent("value");
-
-        for (JsonNode j : array) {
-            int jId = j.path("id").asInt();
-            for (JsonNode v : valuesArray) {
-                System.out.println(jId);
-                int vId = v.path("id").asInt();
-                System.out.println(vId);
-                if (jId == vId) {
-                    String updatedValue = v.path("value").asText();
-                    System.out.println(updatedValue);
-                    ((ObjectNode) j).put("value", updatedValue);
-                    System.out.println(j);
-                }
-            }
-        }
-        String result = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(array);
+        inputValues(testsArray, valuesArray);
+        String result = mapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(testsArray);
         FileDataWriter.write(result);
         System.out.println(result);
     }
 
-    public static Map<Integer, String> convertListToMap(List<Value> values) {
-        return values.stream().collect(Collectors.toMap(Value::getId, Value::getValue));
+    private static void inputValues(List<JsonNode> testsArray,
+                                    List<JsonNode> valuesArray) throws JsonProcessingException {
+        for (JsonNode j : testsArray) {
+            int jId = j.path("id").asInt();
+            for (JsonNode v : valuesArray) {
+                int vId = v.path("id").asInt();
+                if (jId == vId) {
+                    String updatedValue = v.path("value").asText();
+                    ((ObjectNode) j).put("value", updatedValue);
+                }
+            }
+        }
     }
 }
--- src/main/java/chulkova/parser/models/Value.java
+++ src/main/java/chulkova/parser/models/Value.java
@@ -1,45 +1,17 @@
 package chulkova.parser.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
 public class Value {
+
     private Integer id;
     private String value;
-    @JsonProperty(value = "values")
-    List<Value> values;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    public List<Value> getValues() {
-        return values;
-    }
-
-    public void setValues(List<Value> values) {
-        this.values = values;
-    }
-
-    @Override
-    public String toString() {
-        return "Value{" +
-                "id=" + id +
-                ", value='" + value + '\'' +
-                ", values=" + values +
-                '}';
-    }
+    private List<Value> values;
 }
